22ccae6343da4790cb704827140b8cbd96009ad9
unknown <bhbhati@MININT-I44E6DU.fareast.corp.microsoft.com>
2015-05-31 01:43:07 +0530

    first view of extension

---
+++ extension/scripts/tabutilities.js
@@ -0,0 +1,26 @@
+function loadTabsInfo() {
+	// Get All the tabs opened in the browser
+	chrome.tabs.query({}, function(tabs) {
+		for (var index = 0; index < tabs.length; index++) {
+			var tab = tabs[index];
+			// Icon 		
+			var tdFavIcon = $('<td/>');
+			var img = $('<img/>').attr('src', tab.favIconUrl).addClass('tabs-link-img');
+			tdFavIcon.append(img);	
+			
+			// Title		
+			var tdTabTitle = $('<td/>').text(tab.title);
+			
+			var tr = $('<tr/>');
+			tr.append(tdFavIcon);
+			tr.append(tdTabTitle);
+			
+			$("#activeTabsInPopUpPage").append(tr);	
+		}
+	});
+}
+
+// Call functions when the extension popup dom is loaded
+document.addEventListener('DOMContentLoaded', function() {
+	loadTabsInfo();
+});
