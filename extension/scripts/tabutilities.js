733acb4b83406ea351db48e4549a5b300572d107
unknown <bhbhati@MININT-I44E6DU.fareast.corp.microsoft.com>
2015-05-31 17:43:38 +0530

    Make tab active on click

--- extension/scripts/tabutilities.js
+++ extension/scripts/tabutilities.js
@@ -1,33 +1,47 @@
 function loadTabsInfo() {
 	// Get All the tabs opened in the browser
-	chrome.tabs.query({}, function(tabs) {
+	chrome.tabs.query({}, function (tabs) {
 		for (var index = 0; index < tabs.length; index++) {
 			var tab = tabs[index];
 			
+			// Cross
+			var tdCrossIcon = $('<td/>').addClass('tabs-link-img-td-common');
+			var img_cross = $('<img/>').attr('src', 'icons/crossicon16.png').addClass('tabs-link-img-common');
+			tdCrossIcon.append(img_cross);	
+			
 			// Icon 		
-			var tdFavIcon = $('<td/>');
-			var img = $('<img/>').attr('src', tab.favIconUrl).addClass('tabs-link-img');
-			tdFavIcon.append(img);	
+			var tdFavIcon = $('<td/>').addClass('tabs-link-img-td-common');
+			var img_favicon = $('<img/>').attr('src', tab.favIconUrl).addClass('tabs-link-img-common');
+			tdFavIcon.append(img_favicon);	
 			
 			// Title		
 			var tdTabTitle = $('<td/>').text(tab.title);
 			
 			// Current Selected Tab
-			if(tab.active) {
+			if (tab.active) {
 				tdTabTitle.addClass('makeTextBold');
 			}
-			
+
 			var tr = $('<tr/>').attr('tab-id', tab.id);
-			
+
+			tr.append(tdCrossIcon);
 			tr.append(tdFavIcon);
 			tr.append(tdTabTitle);
-			
-			$("#activeTabsInPopUpPage").append(tr);	
+
+			$("#activeTabsInPopUpPage").append(tr);
 		}
 	});
 }
 
 // Call functions when the extension popup dom is loaded
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 	loadTabsInfo();
+	
+	// On Click event on the table row
+	$("table").delegate("tr", "click", function () {
+		var tab_id = $(this).attr('tab-id');
+		// On click make the tab active
+		chrome.tabs.update(parseInt(tab_id), { active: true });
+	});
+
 });
